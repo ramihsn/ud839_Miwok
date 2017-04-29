203dc0637ea3efa1b9a63df72d3a73602f871d35
Rami LT <hsn.rami@gmail.com>
2017-04-29 14:50:06 +0300

    create ListAdapter and ListView for NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.miwok;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,11 +17,8 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
         ArrayList<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")) ;
-        LinearLayout numbersActivity = (LinearLayout) findViewById(R.id.numbers_activity);
-        for(String number: words){
-            TextView tmp = new TextView(this);
-            tmp.setText(number);
-            numbersActivity.addView(tmp);
-        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
