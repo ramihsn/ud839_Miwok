1e2982a5345155b6ca751969789e40a50af03349
Rami LT <hsn.rami@gmail.com>
2017-04-29 12:30:12 +0300

    Temprary commit for debuging

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,39 +19,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.numbers).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.numbers).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, NumbersActivity.class));
             }
         });
 
-        findViewById(R.id.colors).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.colors).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, ColorsActivity.class));
             }
         });
 
-        findViewById(R.id.family).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.family).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, FamilyActivity.class));
             }
         });
 
-        findViewById(R.id.phrases).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.phrases).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, PhrasesActivity.class));
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,16 +2,23 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
-    private final String THIS = "NumbersActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
         ArrayList<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")) ;
+        LinearLayout numbersActivity = (LinearLayout) findViewById(R.id.numbers_activity);
+        for(String number: words){
+            TextView tmp = new TextView(this);
+            tmp.setText(number);
+            numbersActivity.addView(tmp);
+        }
     }
 }
